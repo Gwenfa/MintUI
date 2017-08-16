916f9679d348e940d011e99708795fed96be7890
Gwenfa <Gwenfa>
2017-08-16 20:58:42 +0800

    0816searchbar

--- src/components/home.vue
+++ src/components/home.vue
@@ -4,12 +4,13 @@
   <mt-tab-container-item id="外卖">
     <div class="homeContainer">
 		<header>
-			<mt-search v-model="value"></mt-search>
+			 <mt-search autofocus  v-model="value" :result="filterResult" ></mt-search>
+       <span><i class="fa fa-microphone" ></i></span>
 		</header>
 </div>
   </mt-tab-container-item>
   <mt-tab-container-item id="订单">
-    <mt-cell v-for="n in 5" title="tab-container 2"></mt-cell>
+     <i class="fa fa-microphone" aria-hidden="true"></i>
   </mt-tab-container-item>
   <mt-tab-container-item id="发现">
     <mt-cell v-for="n in 7" title="tab-container 3"></mt-cell>
@@ -43,10 +44,29 @@
 </template>
 
 <script>
+import {api} from '@/global/api'
 export default {
   data () {
     return {
        selected:"外卖",
+       value:"",
+       defaultResult:[]
+    }
+   },
+   mounted(){
+      var vm=this;
+      vm.getaData();
+   },
+   computed: {  
+    filterResult() {  
+      return this.defaultResult.filter(value => new RegExp(this.value, 'i').test(value));  
+    }  
+  },
+   methods:{
+    getaData(){
+      this.$http.get(api.homeData).then(function(response){
+        this.defaultResult=response.data.defaultResult;
+      })
     }
    }
 
@@ -59,6 +79,25 @@ body{
 }
 header{
 	height: 100px;
-	background: #00aaee;
+	background: #1e82d2;
+}
+header .mint-searchbar{
+  background: #1e82d2; 
+  position: inherit;
+}
+header .mint-searchbar-core,header .mint-searchbar-inner {
+  background:#ababab; 
+  height: 1.2rem;
+}
+header .mint-search {
+    height: auto; 
+    float: left;
+}
+span .fa{
+    margin-top: 0.8rem;
+    margin-left: -9.2rem;
+  } 
+header .mint-searchbar .mint-searchbar-cancel{
+  display: none;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import VueResource from 'vue-resource'
+
 import Hello from '@/components/Hello'
 import Test from './../components/test'
 import Home from './../components/home'
 Vue.use(Router)
+Vue.use(VueResource)
 
 export default new Router({
   routes: [
