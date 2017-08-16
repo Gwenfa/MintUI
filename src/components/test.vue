e39ff3c62118dfba00530848257dd82ab380d4a5
unknown <1105233071@qq.com>
2017-08-16 08:54:49 +0800

    0816

---
+++ src/components/test.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+   <mt-button type="default" @click="toast(1,2)">Click me</mt-button>
+   <mt-button type="primary" @click="indicator()">primary</mt-button>
+   <mt-button type="danger">danger</mt-button>
+</div>
+</template>
+<script>
+import { Toast,Indicator } from 'mint-ui';
+export default {
+  data () {
+    return {
+     
+    }
+  },
+  methods:{
+  	toast:function(a,b){
+  		// this.$toast('upload complete')//没有导入Toast时可以这样使用
+  		Toast('提示信息');
+  	},
+  	indicator:function(){
+  	Indicator.open('加载中...');
+  }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
